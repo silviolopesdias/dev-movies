2fc5be2d71afd9a43dc88ae6bfcb0754973d446c
silviolopesdias <silvio.sandra@live.com>
2024-07-18 00:00:13 -0300

    criação do componente Button

---
+++ src/components/Button/index.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import PropTypes from 'prop-types'
+import { ButtonRed, ButtonWhite } from './styles'
+
+function Button({ children, red }) {
+
+
+    return (
+        <>{red ? (
+        <ButtonRed>{children}</ButtonRed>) : (
+        <ButtonWhite>{children}</ButtonWhite>
+        )}
+
+        </>
+    )
+}
+
+export default Button
+
+
+Button.propTypes = {
+    children: PropTypes.string,
+}
+
+Button.propTypes = {
+    red: PropTypes.string
+    
+}
--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -1,7 +1,8 @@
 import React from "react"
 import { useState, useEffect } from "react"
 import api from "../../services/api"
-import { Background } from "./styles"
+import { Background, Info, Poster, Container, ContainerButtons } from "./styles"
+import Button from "../../components/Button"
 
 
 function Home() {
@@ -28,9 +29,19 @@ function Home() {
             {movie && (
 
                 <Background img={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}>
-
-                    <h1>{movie.title}</h1>
-                    <p>{movie.overview}</p>
+                    <Container>
+                        <Info>
+                            <h1>{movie.title}</h1>
+                            <p>{movie.overview}</p>
+                            <ContainerButtons>
+                                <Button red={true}>Assista Agora</Button>
+                                <Button red={false}>Assista o Trailer</Button>
+                            </ContainerButtons>
+                        </Info>
+                        <Poster>
+                            <img alt="capa-do-filme" src={`https://image.tmdb.org/t/p/original${movie.poster_path}`} />
+                        </Poster>
+                    </Container>
                 </Background>
             )}
         </>
